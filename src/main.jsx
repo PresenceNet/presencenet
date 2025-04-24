0af11b60f5ee882b392dce1767543e592726f043
Richard Maruss <rzm5595@psu.edu>
2025-04-24 15:47:34 -0700

    Add full external module list for Vercel Vite build

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,32 @@ export default defineConfig({
         'module',
         'node:module',
         'crypto',
-        'node:crypto'
+        'node:crypto',
+        'tty',
+        'events',
+        'assert',
+        'net',
+        'http',
+        'https',
+        'os',
+        'child_process',
+        'zlib',
+        'buffer',
+        'stream',
+        'querystring',
+        'worker_threads',
+        'node:os',
+        'node:child_process',
+        'node:dns',
+        'node:buffer',
+        'node:assert',
+        'node:process',
+        'node:v8',
+        'node:http',
+        'node:https',
+        'node:http2',
+        'node:zlib',
+        'node:readline'
       ]
     }
   }
